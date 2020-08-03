9a6160b5772dd27bc63508f580926f13e42489c4
miguelmoreirabandtec <miguel.moreira@bandtec.com.br>
2020-08-03 14:20:27 -0300

    🚧 Construção da página de erro

---
+++ src/components/Page-404/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Err } from './styles';
+
+
+function PageFour() {
+    return (
+        <Err>
+            <img src='https://images.tcdn.com.br/img/img_prod/731014/pin_icebrg_zeca_pagodinho_113_1_20200507002900.png'/>          
+        </Err>
+    );
+}
+
+export default PageFour;
--- src/index.js
+++ src/index.js
@@ -8,8 +8,11 @@ import CadastroVideo from './pages/cadastro/Videor';
 import CadastroCategoria from './pages/cadastro/Categoria';
 
 
+
 // Desafio master blaster na descrição
-const Pagina404 = () => (<div>Página 404</div>)
+const Pagina404 = () => (<div> Not Found
+     <img src="https://images.tcdn.com.br/img/img_prod/731014/pin_icebrg_zeca_pagodinho_113_1_20200507002900.png" />
+     </div>)
 
 ReactDOM.render(
     < BrowserRouter >
@@ -17,7 +20,7 @@ ReactDOM.render(
             < Route path="/" component={Home} exact />
             < Route path="/cadastro/video" component={CadastroVideo} exact />
             < Route path="/cadastro/categoria" component={CadastroCategoria} exact />
-            < Route component={Pagina404}/>
+            < Route component={Pagina404} />
         </Switch>
     </BrowserRouter>,
     document.getElementById('root')
